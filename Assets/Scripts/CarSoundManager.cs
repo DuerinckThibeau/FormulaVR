8bdfdfb97948cbbf782b6b67d2e20483e8fde0e1
MoLaazibi <mohamedlaazibi13@gmail.com>
2024-06-17 19:04:15 +0200

    Add CarSounds and CrowdCheering

--- Assets/Scripts/CarSoundManager.cs
+++ Assets/Scripts/CarSoundManager.cs
@@ -7,20 +7,21 @@ public class CarSoundManager : MonoBehaviour
 	public Rigidbody carRigidbody;
 	public float speedThreshold = 1.0f;
 	public float transitionSpeed = 0.1f;
+	private bool gameStarted = false;
 
 	void Start()
 	{
 		idleAudioSource.loop = true;
 		drivingAudioSource.loop = true;
 
-		idleAudioSource.Play();
-		drivingAudioSource.Play();
-
+		idleAudioSource.volume = 0;
 		drivingAudioSource.volume = 0;
 	}
 
 	void Update()
 	{
+		if (!gameStarted) return;
+
 		float speed = carRigidbody.velocity.magnitude;
 
 		if (speed < speedThreshold)
@@ -33,5 +34,18 @@ public class CarSoundManager : MonoBehaviour
 			idleAudioSource.volume = Mathf.Lerp(idleAudioSource.volume, 0, Time.deltaTime * transitionSpeed);
 			drivingAudioSource.volume = Mathf.Lerp(drivingAudioSource.volume, 1, Time.deltaTime * transitionSpeed);
 		}
+	}
+
+	public void StartGame()
+	{
+		idleAudioSource.Play();
+		drivingAudioSource.Play();
+		gameStarted = true;
+	}
+
+	public void MuteSounds()
+	{
+		idleAudioSource.volume = 0;
+		drivingAudioSource.volume = 0;
 	}
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,63 +4,105 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] WheelController car = null;
+	[SerializeField] WheelController car = null;
+	[SerializeField] Transform spawn = null;
+	[SerializeField] TrackCheckpoints trackCheckpoints = null;
+	[SerializeField] GameObject pauseMenu = null;
+	[SerializeField] GameObject startMenu = null;
+	[SerializeField] AudioSource idleAudioSource = null;
+	[SerializeField] AudioSource drivingAudioSource = null;
+	[SerializeField] Rigidbody carRigidbody = null;
+	[SerializeField] float speedThreshold = 1.0f;
 
-    [SerializeField] Transform spawn = null;
+	bool isPaused = false;
+	bool startGame = false;
+	bool gameStarted = false;
 
-    [SerializeField] TrackCheckpoints trackCheckpoints = null;
+	void Start()
+	{
+		idleAudioSource.loop = true;
+		drivingAudioSource.loop = true;
 
-    [SerializeField] GameObject pauseMenu = null;
-    [SerializeField] GameObject startMenu = null;
-    [SerializeField] CarSoundManager carSoundManager = null;
-    
-    bool isPaused = false;
-    bool startGame = false;
+		idleAudioSource.volume = 0;
+		drivingAudioSource.volume = 0;
+	}
 
-    void Start()
-    {
-        
-    }
+	void Update()
+	{
+		startMenu.SetActive(!startGame);
+		if (startGame)
+		{
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+				isPaused = !isPaused;
+			}
+			Time.timeScale = isPaused ? 0 : 1;
+			pauseMenu.SetActive(isPaused);
 
-    void Update()
-    {
-      startMenu.SetActive(!startGame);
-      if(startGame) {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-          isPaused = !isPaused;
-        }
-        Time.timeScale = isPaused ? 0 : 1;
-        pauseMenu.SetActive(isPaused);
-      } else {
-        Time.timeScale = 0;
-      }
-    }
+			if (gameStarted)
+			{
+				UpdateCarSounds();
+			}
+		}
+		else
+		{
+			Time.timeScale = 0;
+		}
+	}
 
+	void UpdateCarSounds()
+	{
+		float speed = carRigidbody.velocity.magnitude;
 
-    public void UnpauseGame() {
-      if(isPaused) {
-        isPaused = false;
-      }
-    }
-    public void StartGame() {
-      if(!startGame) {
-        startGame = true;
-        Time.timeScale = 1;
-      }
-    }
-    public void ExitGame() {
-      Application.Quit();
-    }
+		if (speed < speedThreshold)
+		{
+			idleAudioSource.volume = 1;
+			drivingAudioSource.volume = 0;
+		}
+		else
+		{
+			idleAudioSource.volume = 0;
+			drivingAudioSource.volume = 1;
+		}
+	}
 
-    public void ResetGame() 
-    { 
-        Transform transform = car.GetComponent<Transform>();
-        transform.position = spawn.position;
-        transform.forward = spawn.forward;
-        trackCheckpoints.ResetCheckpoint(transform);
-        car.StopCompletely();
+	public void StartGame()
+	{
+		if (!startGame)
+		{
+			startGame = true;
+			Time.timeScale = 1;
+			gameStarted = true;
+			idleAudioSource.Play();
+			drivingAudioSource.Play();
+		}
+	}
 
-    }
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
 
+	public void ResetGame()
+	{
+		Transform transform = car.GetComponent<Transform>();
+		transform.position = spawn.position;
+		transform.forward = spawn.forward;
+		trackCheckpoints.ResetCheckpoint(transform);
+		car.StopCompletely();
+	}
+
+	public void MuteAllSounds()
+	{
+		idleAudioSource.volume = 0;
+		drivingAudioSource.volume = 0;
+	}
+
+	public void UnpauseGame()
+	{
+		if (isPaused)
+		{
+			isPaused = false;
+		}
+	}
 }
